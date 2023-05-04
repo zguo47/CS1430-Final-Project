3aa2c4eff70216ad10bdf572c1f43dcabfdf9dab
lliu58b <linghai_liu@brown.edu>
2023-05-04 00:52:53 -0400

    for cluster

--- functions.py
+++ functions.py
@@ -9,7 +9,7 @@ def loss_with_metrics(img_ab_out, img_ab_true, name=""):
     # summary = tf.summary.scalar("cost " + name, cost)
     return loss(img_ab_out, img_ab_true)
 
-def training_loop(col, train_steps, optimizer, dataloader, learning_rate):
+def training_loop(col, optimizer, dataloader, device):
     # Set up training (input queues, graph, optimizer)
     # irr = LabImageRecordReader("lab_images_*.tfrecord", dir_tfrecord)
     # read_batched_examples = irr.read_batch(batch_size, shuffle=True)
@@ -18,6 +18,8 @@ def training_loop(col, train_steps, optimizer, dataloader, learning_rate):
     col.train()
     for batch_idx, img in enumerate(dataloader):
       imgs_l, imgs_true_ab = img
+      imgs_l = imgs_l.to(device)
+      imgs_true_ab = imgs_true_ab.to(device)
       optimizer.zero_grad()
       imgs_ab = col.build(imgs_l) 
       batch_loss = loss_with_metrics(imgs_ab, imgs_true_ab, "training")
@@ -25,13 +27,13 @@ def training_loop(col, train_steps, optimizer, dataloader, learning_rate):
         print(batch_loss)
       batch_loss.backward()
       optimizer.step()
-      loss_list.append(batch_loss.item())
+      loss_list.append(batch_loss.detach().cpu().numpy())
       optimizer.step()
     return loss_list
  
 
 
-def evaluation_loop(col, dataloader):
+def evaluation_loop(col, dataloader, device):
     # Set up validation (input queues, graph)
     # irr = LabImageRecordReader("val_lab_images_*.tfrecord", dir_tfrecord)
     # read_batched_examples = irr.read_batch(number_of_images, shuffle=False)
@@ -39,8 +41,10 @@ def evaluation_loop(col, dataloader):
     col.eval()
     for batch_idx, img in enumerate(dataloader):
       imgs_l_val, imgs_true_ab_val = img
+      imgs_l_val = imgs_l_val.to(device)
+      imgs_true_ab_val = imgs_true_ab_val.to(device)
       imgs_ab_val = col.build(imgs_l_val)
       batch_loss = loss_with_metrics(imgs_ab_val, imgs_true_ab_val, "validation")
       batch_loss.backward()
-      loss_list.append(batch_loss.item())
+      loss_list.append(batch_loss.detach().cpu().numpy())
     return loss_list
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from torch.utils.data import Dataset, DataLoader
 import torch
 import torch.nn as nn
 from torchvision import transforms, utils
-from skimage import io, transform
 import numpy as np
 
 from model import Colorization
@@ -10,23 +9,27 @@ from dataset import Dataset1
 from functions import training_loop, evaluation_loop
 
 def main():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-
-    col = Colorization(256)
-
-    val_number_of_images = 10
+    col = Colorization(256).to(device)
     batch_size = 100
     learning_rate = 0.001
-    train_steps = 100
     num_epochs = 20
     optimizer =  torch.optim.Adam(col.parameters(), lr=learning_rate)
 
-    train_gray_dir = "/content/drive/Shareddrives/CVFinalProject/data/dataset1/train_black/"
-    train_color_dir = "/content/drive/Shareddrives/CVFinalProject/data/dataset1/train_color/"
-    test_gray_dir = "/content/drive/Shareddrives/CVFinalProject/data/dataset1/test_black/"
-    test_color_dir = "/content/drive/Shareddrives/CVFinalProject/data/dataset1/test_color/"
+    # On google drive
+    # train_gray_dir = "/content/drive/Shareddrives/CVFinalProject/data/dataset1/train_black/"
+    # train_color_dir = "/content/drive/Shareddrives/CVFinalProject/data/dataset1/train_color/"
+    # test_gray_dir = "/content/drive/Shareddrives/CVFinalProject/data/dataset1/test_black/"
+    # test_color_dir = "/content/drive/Shareddrives/CVFinalProject/data/dataset1/test_color/"
+    
+    # On computing cluster
+    train_gray_dir = "/users/lliu58/scratch/data/train_black/"
+    train_color_dir = "/users/lliu58/scratch/data/train_color/"
+    test_gray_dir = "/users/lliu58/scratch/data/test_black/"
+    test_color_dir = "/users/lliu58/scratch/data/test_color/"
 
-    output_size = (224, 224)
+    output_size = (128, 128)
     batch_size = 16
 
     transform = transforms.Compose(
@@ -42,8 +45,11 @@ def main():
     test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
 
     for epoch in range(num_epochs):
-        train_losses = training_loop(col, train_steps, optimizer, train_dataloader, learning_rate)
-        test_losses = evaluation_loop(col, test_dataloader)
+        train_losses = training_loop(col, optimizer, train_dataloader, device)
+        test_losses = evaluation_loop(col, test_dataloader, device)
         epoch_train_loss = np.mean(train_losses)
         epoch_test_loss = np.mean(test_losses)
         print(f"Epoch {epoch+1}, avg train loss {epoch_train_loss:.3f}, avg test loss {epoch_test_loss:.3f}")
+
+if __name__ == "__main__":
+    main()
